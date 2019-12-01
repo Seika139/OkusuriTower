6c7b1df3043342024e3710cde6953ffe9d0fdf0f
Seika139 <tcoanzu@gmail.com>
2019-12-01 15:17:02 +0900

    initial commit

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Director : MonoBehaviour {
+
+
+    private GameObject currentDrug;
+    DrugController drugController;
+
+    public int score;
+    public GameObject scoreObj;
+    public GameObject currentDrugObj;
+
+    public GameObject[] drugs;
+
+    private float span = 1.5f;
+    private float delta = 0;
+
+    private string[] drugNames = {
+        "アセチルコリン",
+        "ニコチン",
+        "バンコマイシン",
+        "レベチラセタム",
+        "ジアゼパム",
+        "ペニシリン"
+    };
+
+	// Use this for initialization
+	void Start () {
+        score = 1;
+
+        int number = Random.Range(0, drugs.Length);
+        currentDrug = Instantiate(drugs[number], transform.position, transform.rotation);
+        currentDrugObj.GetComponent<Text>().text = drugNames[number];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        drugController = currentDrug.GetComponent<DrugController>();
+        bool isMoving = drugController.isMoving;
+        bool canMove = drugController.canMove;
+        scoreObj.GetComponent<Text>().text = score.ToString();
+        if (isMoving == false && canMove==false)
+        {
+            delta += Time.deltaTime;
+            if (delta > span)
+            {
+                score++;
+                delta = 0;
+                int number = Random.Range(0, drugs.Length);
+                currentDrug = Instantiate(drugs[number], transform.position, transform.rotation);
+                currentDrugObj.GetComponent<Text>().text = drugNames[number];
+            }
+
+        }
+    }
+
+    public void Rotate()
+    {
+        if (drugController.canMove == true)
+        {
+            currentDrug.transform.Rotate(new Vector3(0, 0, -30));
+        }
+    }
+}
---
+++ Assets/Scripts/DrugController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DrugController : MonoBehaviour {
+
+    private Rigidbody2D rgbd;
+    public float velocity;
+
+    // 位置座標
+    private Vector3 position;
+    // スクリーン座標をワールド座標に変換した位置座標
+    private Vector3 screenToWorldPointPosition;
+
+
+    public bool canMove;
+    public bool isMoving;
+
+    // Use this for initialization
+    void Start () {
+        rgbd = this.GetComponent<Rigidbody2D>();
+        rgbd.bodyType = RigidbodyType2D.Static;
+        canMove = true;
+        isMoving = false;
+        transform.position = new Vector3(0, 3, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = rgbd.velocity.magnitude;
+        // ボタンを押した時に画面のタッチを無視したい
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            if (Input.GetMouseButton(0) && canMove == true)
+            {
+                position = Input.mousePosition;
+                // マウス位置座標をスクリーン座標からワールド座標に変換する
+                screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+                // ワールド座標に変換されたマウス座標のx座標を代入
+                gameObject.transform.position = new Vector3(screenToWorldPointPosition.x, 3, 0);
+            }
+            else if (Input.GetMouseButtonUp(0) && canMove == true)
+            {
+                rgbd.bodyType = RigidbodyType2D.Dynamic;
+                canMove = false;
+                isMoving = true;
+            }
+        }
+        if (isMoving==true && transform.position.y<2.5 && velocity<1.0e-3)
+        {
+            isMoving = false;
+        }
+    }
+
+
+}
